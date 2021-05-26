4f742267ea2e795e09395691dd3df373266d0e76
yemre-aydin <isteyenler_icin@hotmail.com>
2021-05-26 20:05:16 +0300

    2605

---
+++ 01_python_giris/0305_lab.py
@@ -0,0 +1,20 @@
+
+secim=input('uygulamadan çıkmak için "ç"')
+i=1
+
+ort=0
+while secim !="ç":
+    vize=int(input(f"{i}. öğrencinin vize notunu girin:"))
+    final=int(input(f"{i}. öğrencinin final notunu girin:"))
+    i+=1
+    secim=input('uygulamadan çıkmak için "ç"')
+    ort+=(vize+final)/2
+print(i)
+genelort=ort/(i-1)    
+print(genelort)
+
+
+
+
+    
+
